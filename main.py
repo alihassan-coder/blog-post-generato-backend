e36d842478baf40636c42021a679707d30bd0407
alihassan-coder <alibinsalman786@gmail.com>
2025-09-09 10:47:21 -0700

    fix main.py fix for deploy

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 from fastapi import FastAPI
-from mangum import Mangum
+from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
+from config.database_config import client, ALLOWED_ORIGINS
+from routes import auth, chat
+from routes import agent as agent_routes
+
 
 app = FastAPI()
 
 @app.get("/")
-def home():
-    return {"message": "FastAPI running on Vercel 🚀"}
+def root():
+    return {"message": "AI Blog Generator API is running!"}
 
-# Wrap FastAPI for serverless
-handler = Mangum(app)
+
+# Middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=ALLOWED_ORIGINS,  # list of allowed origins
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Routes
+app.include_router(auth.router)
+app.include_router(chat.router)
+app.include_router(agent_routes.router)
+
+
+
